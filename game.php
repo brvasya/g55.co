7de56c9d5883ef551b7ba1b7b6f14adcdb261cfc
brvasya <crazygames888@gmail.com>
2026-01-23 20:54:53 +0300

    add

--- game.php
+++ game.php
@@ -52,7 +52,7 @@ gtag('config', 'G-BV72Y8RMLN');
 </div>
 <ul id="tower_r" class="block"><li id="ads" class="block"><script async src="/js/336x280.js"></script></li>
 <?php foreach ($similar as $p): ?>
-<li><a class="tag" style="background-image: url(<?php echo h('/wp-content/uploads/' . $p['id'] . '.png'); ?>);" href="/game.php?id=<?php echo rawurlencode($p['id']); ?>&c=<?php echo rawurlencode($cid); ?>" title="<?php echo h($p['title']); ?>" target="_top"><?php echo h($p['title']); ?></a></li>
+<li><a class="tag" style="background-image: url(<?php echo h('https://cdn.g55.co/wp-content/uploads/' . $p['id'] . '.png'); ?>);" href="/game.php?id=<?php echo rawurlencode($p['id']); ?>&c=<?php echo rawurlencode($cid); ?>" title="<?php echo h($p['title']); ?>" target="_top"><?php echo h($p['title']); ?></a></li>
 <?php endforeach; ?>
 <li><a class="tag" id="<?php echo rawurlencode($cid); ?>" href="<?php echo h($moreHref); ?>" title="<?php echo h($moreText); ?>" target="_top"><?php echo h($moreText); ?></a></li>
 </ul>
