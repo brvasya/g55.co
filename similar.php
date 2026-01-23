37e0cc0a88f973042b40ef7858d1e49cf58180b1
brvasya <crazygames888@gmail.com>
2026-01-23 04:37:06 +0300

    Update similar.php

--- similar.php
+++ similar.php
@@ -1,7 +1,5 @@
 <li id="ads" class="block"><script async src="/ads/336x280.js"></script></li>
-<?php $category = get_the_category(); query_posts(array('showposts' => 6, 'orderby' => 'rand', 'category_name' => $category[0]->name, 'post__not_in' => array($post->ID))); ?>
-<?php while (have_posts()) : the_post(); ?>
-<li><a class="tag" style="background-image: url(<?php echo get_post_meta($id, 'wpcf-image-jpg', true); ?>);" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" target="_top"><?php the_title(); ?></a></li>
-<?php endwhile; ?>
-<li><a class="tag" id="<?php echo $category[0]->slug; ?>" href="<?php echo get_category_link($category[0]->term_id); ?>" title="More <?php echo $category[0]->name; ?> Games" target="_top">More <?php echo $category[0]->name; ?> Games</a></li>
-<?php wp_reset_query(); ?>
+<?php foreach ($similar as $p): ?>
+<li><a class="tag" style="background-image: url(<?php echo h('/wp-content/uploads/' . $p['id'] . '.png'); ?>);" href="/game.php?id=<?php echo rawurlencode($p['id']); ?>&c=<?php echo rawurlencode($cid); ?>" title="<?php echo h($p['title']); ?>" target="_top"><?php echo h($p['title']); ?></a></li>
+<?php endforeach; ?>
+<li><a class="tag" id="<?php echo rawurlencode($cid); ?>" href="<?php echo h($moreHref); ?>" title="<?php echo h($moreText); ?>" target="_top"><?php echo h($moreText); ?></a></li>
