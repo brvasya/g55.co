68ee1997569f407802b7bf5f8bffa34173d07072
brvasya <crazygames888@gmail.com>
2026-01-26 01:15:32 +0300

    Update game.php

--- game.php
+++ game.php
@@ -70,7 +70,7 @@ gtag('config', 'G-BV72Y8RMLN');
 <table id="menu">
 <tr>
 <td>
-<h2>Discover All Games</h2>
+<h3>Discover All Games</h3>
 <ul class="menu">
 <?php foreach ($categories as $c): ?>
 <li><a class="tag" id="<?php echo rawurlencode($c['id']); ?>" href="/?c=<?php echo rawurlencode($c['id']); ?>"><?php echo h($c['name']); ?></a></li>
