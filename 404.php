4403d02f18f30ff119ffd4ffcb1c5554fb70e375
brvasya <crazygames888@gmail.com>
2026-01-25 09:26:25 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -10,6 +10,6 @@
 <div class="background"></div>
 <div class="logo"></div>
 <div class="progress" style="padding: 35px; text-align: center;">This game is not available, click play more games to discover best games by g55.co</div>
-<a class="play" href="https://g55.co/?utm_source=404&utm_medium=404&utm_campaign=404" target="_blank">&#9654; Play More Games</a>
+<a class="play" href="https://g55.co/" target="_blank">&#9654; Play More Games</a>
 </body>
 </html>
