21a10a4eae1f6119088ae3e720dab3199511ca18
brvasya <crazygames888@gmail.com>
2026-01-25 05:12:44 +0300

    Update ads-generating-js.php

--- ads-generating-js.php
+++ ads-generating-js.php
@@ -1,3 +1,3 @@
-<?php $rss = file_get_contents('https://adbpage.com/adblock?v=3&format=js'); echo $rss; ?>
+<?php echo file_get_contents('https://adbpage.com/adblock?v=3&format=js'); ?>
 
 aclib.runAutoTag({zoneId: 'nitppchfag'});
