c18444532dc705537283e4f216c5eadd71b76ddc
brvasya <crazygames888@gmail.com>
2026-01-26 04:28:08 +0300

    add

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'inde
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 <script src="/js/mason.min.js"></script>
 <script async src="https://cse.google.com/cse.js?cx=f088a66cef0354852"></script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6180203036822393" crossorigin="anonymous"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4677496585017452" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-BV72Y8RMLN"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
