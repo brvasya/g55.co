9e150dd118c96b545b8c98fca3b86dea047dd33c
brvasya <crazygames888@gmail.com>
2026-01-29 00:17:50 +0300

    add

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
 <link rel="stylesheet" href="/css/style.css">
 <link rel="preload" href="/css/icons.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-<script src="/js/mason.min.js"></script>
 <script async src="https://cse.google.com/cse.js?cx=f088a66cef0354852"></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4677496585017452" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-BV72Y8RMLN"></script>
@@ -44,6 +42,5 @@ gtag('config', 'G-BV72Y8RMLN');
 <?php endforeach; ?>
 </div>
 <?php include 'footer.php'; ?>
-<script src="/js/resize.js"></script>
 </body>
 </html>
