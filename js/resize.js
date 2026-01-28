284586b411dacdca6b04267be36eea172eb4e0e3
brvasya <crazygames888@gmail.com>
2026-01-28 13:44:23 +0300

    Update resize.js

--- js/resize.js
+++ js/resize.js
@@ -4,8 +4,7 @@ function resize() {
 		ratio: 1.31,
 		sizes: [[1,1]],
 		columns: [[0,480,2],[480,780,4],[780,1080,5],[1080,1320,6],[1320,1680,8]],
-		layout: "fluid",
-		gutter: 5
+		layout: "fluid"
 	});
 }
 document.addEventListener("DOMContentLoaded", function() { resize(); });
