5681fd166027bac50181bd38249e619b1df4065e
brvasya <crazygames888@gmail.com>
2026-01-30 04:24:04 +0300

    Update head.php

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="/css/style.css">
 <link rel="preload" href="/css/icons.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <script async src="https://cse.google.com/cse.js?cx=f088a66cef0354852"></script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6180203036822393" crossorigin="anonymous"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4677496585017452" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-BV72Y8RMLN"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
