206bca17315529f8f73693319c47351e735b4660
brvasya <crazygames888@gmail.com>
2026-02-23 15:25:15 +0300

    Update gd.php

--- gd.php
+++ gd.php
@@ -14,7 +14,6 @@ if (!isset($_GET['c']) || !isset($_GET['p'])) {
 }
 
 $category = trim((string)$_GET['c']);
-$category = str_replace('+', '-', $category);
 $page     = (int)$_GET['p'];
 
 $type = isset($_GET['type']) ? trim((string)$_GET['type']) : 'categories';
