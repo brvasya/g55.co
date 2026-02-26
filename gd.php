8b816956f0bd2ae17f8d3b4fd73aafef9a754900
brvasya <crazygames888@gmail.com>
2026-02-26 19:31:30 +0300

    add

--- gd.php
+++ gd.php
@@ -24,6 +24,8 @@ $categoryMap = [
 'jewels'     => 'bejeweled',
 'squidgame'  => 'squid-game',
 'guns'       => 'gun',
+'mahjong%20%26%20connect' => 'mahjong',
+'racing%20%26%20driving'  => 'racing',
 ];
 $category = $categoryMap[$categoryRaw] ?? $categoryRaw;
 $category = str_replace(' ', '-', $category);
