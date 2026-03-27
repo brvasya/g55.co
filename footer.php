c2c199a3ba994ebd802022602e70774b298e50c6
brvasya <crazygames888@gmail.com>
2026-03-27 12:08:01 +0300

    add

--- footer.php
+++ footer.php
@@ -7,18 +7,7 @@
 </ul>
 </nav>
 <footer>
-<div>
 <span>&#169; <?php echo date('Y'); ?> G55.CO</span>
 <a href="/privacy-policy.php">Privacy Policy</a>
 <a href="mailto:crazygames888@gmail.com">Contact Us</a>
-</div>
-<div>
-<a href="https://coloring.g55.co/">Printable Coloring Pages</a>
-<a href="https://coloring.g55.co/?c=animals">Animals Coloring Pages</a>
-<a href="https://coloring.g55.co/?c=minecraft">Minecraft Coloring Pages</a>
-<a href="https://coloring.g55.co/?c=pokemon">Pokemon Coloring Pages</a>
-<a href="https://coloring.g55.co/?c=roblox">Roblox Coloring Pages</a>
-<a href="https://coloring.g55.co/?c=sonic">Sonic Coloring Pages</a>
-<a href="https://coloring.g55.co/?c=mario">Mario Coloring Pages</a>
-</div>
 </footer>
