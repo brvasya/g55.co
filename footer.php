f7b04854fa8674c9a4cc43d2e97406460baed696
brvasya <crazygames888@gmail.com>
2026-07-08 01:11:19 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -7,9 +7,6 @@
 </ul>
 </nav>
 <footer>
+<div><a href="/privacy-policy.php">Privacy Policy</a><a href="mailto:crazygames888@gmail.com">Contact Us</a></div>
 <div>&#169; <?php echo date('Y'); ?> G55.CO</div>
-<div>
-<a href="/privacy-policy.php">Privacy Policy</a>
-<a href="mailto:crazygames888@gmail.com">Contact Us</a>
-</div>
 </footer>
