3de1c173037c817973057976f941a2665486bf62
brvasya <crazygames888@gmail.com>
2026-07-08 01:45:19 +0300

    Update game.php

--- game.php
+++ game.php
@@ -44,9 +44,9 @@
 <?php if ($seriesLinks): ?>
 <nav class="cluster">
 <h2>More <?php echo h($currentSeriesTitle) ?> Games</h2>
-<section class="series">
+<section class="grid series">
 <?php foreach ($seriesLinks as $p): ?>
-<a class="thumbnail" style="background-image: url(<?php echo h('https://cdn.g55.co/' . $p['id'] . '.png'); ?>);" href="/game.php?id=<?php echo rawurlencode($p['id']); ?>&c=<?php echo rawurlencode($cid); ?>"><span class="<?php echo rawurlencode($p['category']); ?>"><?php echo h($p['title']); ?></span></a>
+<a class="thumbnail" style="background-image: url(<?php echo h('https://cdn.g55.co/' . $p['id'] . '.png'); ?>);" href="/game.php?id=<?php echo rawurlencode($p['id']); ?>&c=<?php echo rawurlencode($cid); ?>"><span class="<?php echo rawurlencode($cid); ?>"><?php echo h($p['title']); ?></span></a>
 <?php endforeach; ?>
 </section>
 </nav>
