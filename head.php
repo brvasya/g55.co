f82409d6955b6b8a9d7d8c44a8d0ff03680833db
brvasya <crazygames888@gmail.com>
2026-08-22 19:25:22 +0300

    Update head.php

--- head.php
+++ head.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
 <link rel="stylesheet" href="/css/style.css">
 <link rel="preload" href="/css/icons.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<script defer src="/js/pwa.js"></script>
 <script async src="https://cse.google.com/cse.js?cx=f088a66cef0354852"></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4677496585017452" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-BV72Y8RMLN"></script>
