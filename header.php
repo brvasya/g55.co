19f8156cd0fee87a755ff67441efcdf823cf056b
brvasya <crazygames888@gmail.com>
2026-08-27 18:30:45 +0300

    Update header.php

--- header.php
+++ header.php
@@ -3,3 +3,10 @@
 <div class="gcse-searchbox-only"></div>
 <button class="tag install">Install app</button>
 </header>
+<nav>
+<ul class="categories">
+<?php foreach ($categories as $c): ?>
+<li><a class="tag <?php echo rawurlencode($c['id']); ?>" href="/?c=<?php echo rawurlencode($c['id']); ?>"><?php echo h($c['name']); ?></a></li>
+<?php endforeach; ?>
+</ul>
+</nav>
