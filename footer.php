5b60f3c92c5441bfb0635366718ee0126caa3af5
brvasya <crazygames888@gmail.com>
2026-08-27 18:30:42 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,3 @@
-<nav>
-<h2>Explore All Game Categories</h2>
-<ul class="categories">
-<?php foreach ($categories as $c): ?>
-<li><a class="tag <?php echo rawurlencode($c['id']); ?>" href="/?c=<?php echo rawurlencode($c['id']); ?>"><?php echo h($c['name']); ?></a></li>
-<?php endforeach; ?>
-</ul>
-</nav>
 <footer>
 <div><a href="/privacy-policy.php">Privacy Policy</a><a href="mailto:crazygames888@gmail.com">Contact Us</a></div>
 <div>&#169; <?php echo date('Y'); ?> G55.CO</div>
