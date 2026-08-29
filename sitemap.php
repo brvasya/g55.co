66d96861720a697a4f50ec49bdcaa95a875b8d2d
brvasya <crazygames888@gmail.com>
2026-08-29 16:16:56 +0300

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -22,26 +22,14 @@ $today = date('Y-m-d');
 echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 echo '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
 
-echo "  <sitemap>\n";
-echo "    <loc>" . xml_e($base . "/sitemap_categories.php") . "</loc>\n";
-echo "    <lastmod>" . xml_e($today) . "</lastmod>\n";
-echo "  </sitemap>\n";
+echo "<sitemap><loc>" . xml_e($base . "/sitemap_categories.php") . "</loc><lastmod>" . xml_e($today) . "</lastmod></sitemap>\n";
 
-echo "  <sitemap>\n";
-echo "    <loc>" . xml_e($base . "/sitemap_creators.php") . "</loc>\n";
-echo "    <lastmod>" . xml_e($today) . "</lastmod>\n";
-echo "  </sitemap>\n";
+echo "<sitemap><loc>" . xml_e($base . "/sitemap_creators.php") . "</loc><lastmod>" . xml_e($today) . "</lastmod></sitemap>\n";
 
-echo "  <sitemap>\n";
-echo "    <loc>" . xml_e($base . "/sitemap_series.php") . "</loc>\n";
-echo "    <lastmod>" . xml_e($today) . "</lastmod>\n";
-echo "  </sitemap>\n";
+echo "<sitemap><loc>" . xml_e($base . "/sitemap_series.php") . "</loc><lastmod>" . xml_e($today) . "</lastmod></sitemap>\n";
 
 for ($i = 1; $i <= $pageSitemaps; $i++) {
-  echo "  <sitemap>\n";
-  echo "    <loc>" . xml_e($base . "/sitemap_games.php?n=" . $i) . "</loc>\n";
-  echo "    <lastmod>" . xml_e($today) . "</lastmod>\n";
-  echo "  </sitemap>\n";
+  echo "<sitemap><loc>" . xml_e($base . "/sitemap_games.php?n=" . $i) . "</loc><lastmod>" . xml_e($today) . "</lastmod></sitemap>\n";
 }
 
 echo "</sitemapindex>\n";
