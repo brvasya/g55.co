a0c46448a956d3d2cf22018de22974364284860c
brvasya <crazygames888@gmail.com>
2026-09-09 07:13:09 +0300

    Update game.php

--- game.php
+++ game.php
@@ -32,6 +32,12 @@
 </div>
 <a class="tag <?php echo rawurlencode($cid); ?>" href="/?c=<?php echo rawurlencode($cid); ?>">More <?php echo h($cat['name']); ?> Games</a>
 </aside>
+<?php if (in_array('Exclusive', $page['categories'], true)): ?>
+<div class="embed-code">
+<button class="tag copy" onclick="navigator.clipboard.writeText(document.querySelector('code').textContent); this.innerText='Copied!'; setTimeout(()=>this.innerText='Copy Embed Code',1500);">Copy Embed Code</button>
+<code><?php echo h('<iframe src="' . $iframeSrc . '" width="960" height="600" frameborder="0" scrolling="no" allowfullscreen></iframe>'); ?></code>
+</div>
+<?php endif; ?>
 </article>
 <nav class="pagination">
 <?php if ($prevUrl): ?>
