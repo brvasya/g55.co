df7e61cf42aef904e55ce3992d635f18ae41e223
brvasya <crazygames888@gmail.com>
2026-09-11 01:44:16 +0300

    Update game.php

--- game.php
+++ game.php
@@ -21,7 +21,7 @@
 </section>
 <div class="embed">
 <button class="fullscreen" onclick="document.querySelector('.embed iframe')?.requestFullscreen();" title="Fullscreen"></button>
-<iframe sandbox="allow-scripts allow-same-origin allow-pointer-lock" src="<?php echo h($iframeSrc); ?>" scrolling="no" allowfullscreen></iframe>
+<iframe src="<?php echo h($iframeSrc); ?>" scrolling="no" allowfullscreen></iframe>
 </div>
 <aside class="tower_r">
 <div class="ads"><script async src="/js/336x280.js"></script></div>
