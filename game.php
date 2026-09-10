31cfc4c9797f00567926a1b9a4cadf05e19c2891
brvasya <crazygames888@gmail.com>
2026-09-10 15:28:59 +0300

    Update game.php

--- game.php
+++ game.php
@@ -59,7 +59,7 @@
 <?php endif; ?>
 <?php if ($creatorLinks): ?>
 <nav class="cluster">
-<h2>More <?php echo h($cat['name']); ?> Games by <a href="/?by=<?php echo rawurlencode($currentCreatorTitle); ?>"><?php echo h($currentCreatorTitle); ?></a></h2>
+<h2>More <a href="/?c=<?php echo rawurlencode($cid); ?>"><?php echo h($cat['name']); ?> Games</a> by <a href="/?by=<?php echo rawurlencode($currentCreatorTitle); ?>"><?php echo h($currentCreatorTitle); ?></a></h2>
 <section class="grid">
 <?php foreach ($creatorLinks as $p): ?>
 <a class="thumbnail" style="background-image: url(<?php echo h('https://cdn.g55.co/' . $p['id'] . '.png'); ?>);" href="/game.php?id=<?php echo rawurlencode($p['id']); ?>&c=<?php echo rawurlencode($cid); ?>"><span class="<?php echo rawurlencode($cid); ?>"><?php echo h($p['title']); ?></span></a>
